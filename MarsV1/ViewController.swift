baf6def7dd81ebb48019a67bc942490b7e998e3f
parkjain <elly000417@gmail.com>
2018-05-16 14:28:12 +0900

    Initial Commit

---
+++ MarsV1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarsV1
+//
+//  Created by Release on 2018. 5. 16..
+//  Copyright © 2018년 fredhur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
